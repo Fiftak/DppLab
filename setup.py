d18d871c991baa0c9291bc4d4bed95c164f926ee
fwisniewski234994 <fwisniewski234994@e-science.pl>
2019-06-05 22:59:49 +0200

    Biblioteka numer 1 Aktualizacja numer 7

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="dppPackage1",
-    version="0.0.2",
+    version="0.0.1",
     author="Filip Author",
     author_email="filipow19@gmail.com",
     description="A small example package",
